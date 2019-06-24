8c62362a96abf0eefc46cdb5f58c85d9f88bd13d
a.gubankov <alexg@ukr.net>
2019-06-24 14:41:08 +0300

    32commit

--- src/main/java/MainClassTEsting.java
+++ src/main/java/MainClassTEsting.java
@@ -2,7 +2,7 @@ public class firstTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Rezume already efedd");
+        System.out.println("Rezume already efdvs");
     }
 
 }
