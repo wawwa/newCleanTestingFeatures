3ec538d203a4b6387a3b16139289f6d079c06cea
a.gubankov <alexg@ukr.net>
2019-06-25 18:37:36 +0300

    commit

--- src/main/java/MainClassTEsting.java
+++ src/main/java/MainClassTEsting.java
@@ -6,6 +6,7 @@ public class firstTest {
         System.out.println("Rezume already 331122");
         System.out.println("Rezume already 331122");
         System.out.println("Rezume already 331122");
+        System.out.println("Rezume already 331122");
     }
 
 }
