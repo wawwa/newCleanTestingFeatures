be2a6da65ad4399542f58d98181f57c9ffc344a3
a.gubankov <alexg@ukr.net>
2019-06-25 12:53:40 +0300

    added gitignore

--- src/main/java/MainClassTEsting.java
+++ src/main/java/MainClassTEsting.java
@@ -2,7 +2,7 @@ public class firstTest {
 
     public static void main(String[] args) {
 
-        System.out.println("Rezume already 3311");
+        System.out.println("Rezume already 331122");
     }
 
 }
